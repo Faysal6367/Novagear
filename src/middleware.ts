c7846d1efe8c055af7b5aea91a4421e59943c812
Faysal6367 <faysal.github2002@gmail.com>
2026-07-14 12:38:44 +0600

    (middleware) changed middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth"
 import { authConfig } from "@/auth.config"
-
+// This middleware function is responsible for handling authentication and authorization for specific routes in the application. It uses NextAuth to manage user sessions and access control. The middleware is configured to match certain paths, ensuring that only authenticated users can access those routes.
 export default NextAuth(authConfig).auth
 
 export const config = {
